739a2de298093ec813fc01c72d50a91771e79b42
andong0909 <yanandong@gmail.com>
2025-01-23 18:31:57 +0800

    Add echo

--- src/main/java/Woof.java
+++ src/main/java/Woof.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Woof {
     public static void main(String[] args) {
         // Greeting
         System.out.println("""
                 Woof! Woof! I am your paw-sonsal chatbot!
                 What can Woof do for you?
-                Aww byebye. Don't forget to come back soon
                 """);
+
+        // Echo
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Aww byebye. Don't forget to come back soon");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        sc.close();
     }
 }
