ea922311ad7a9bf7700bfc86ca92fd34fac1b662
andong0909 <yanandong@gmail.com>
2025-01-23 18:25:34 +0800

    Add greeting and exit message

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Woof User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Woof.java
@@ -0,0 +1,10 @@
+public class Woof {
+    public static void main(String[] args) {
+        // Greeting
+        System.out.println("""
+                Woof! Woof! I am your paw-sonsal chatbot!
+                What can Woof do for you?
+                Aww byebye. Don't forget to come back soon
+                """);
+    }
+}
